72e7b06cd7c1dd92f48cfd05c55e447eccdd4d21
hahn2014 <slapshot1998@gmail.com>
2019-06-10 19:12:02 -0700

    Added more details to main menu, Online99 now has board set up.

---
+++ Tetris99/Assets/MainMenuRoomSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuRoomSwitch : MonoBehaviour {
+
+    public void GoToOnline99() {
+        SceneManager.LoadScene("Online99", LoadSceneMode.Single);
+    }
+
+
+    public void GoToOffline99() {
+        SceneManager.LoadScene("Offline99", LoadSceneMode.Single);
+    }
+
+
+    public void GoToOfflineMarathon() {
+        SceneManager.LoadScene("OfflineMarathon", LoadSceneMode.Single);
+    }
+}
