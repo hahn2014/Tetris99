d466bf9201b108304eb84fcc92f4307181c20184
hahn2014 <slapshot1998@gmail.com>
2019-06-10 21:40:17 -0700

    Online99 Now spawns in tetriminoes and they begin to fall. Fall speed and spawn speed are NOT final.

---
+++ Tetris99/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+    private float update;
+    public GameObject[] groups;
+
+    public void spawnNext() {
+        int i = Random.Range(0, groups.Length);
+        Debug.Log(groups[i].name + " has been spawned.");
+        Instantiate(groups[i], transform.position, Quaternion.identity);
+    }
+
+    void Update() {
+        update += Time.deltaTime;
+        if (update > 1.0f) {
+            update = 0.0f;
+            spawnNext();
+        }
+    }
+}
---
+++ Tetris99/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    void Update() {
+        /*if (Input.GetKeyUp(KeyCode.upArrow)) {
+            Debug.Log("Hard Drop");
+        } else if (Input.GetKeyUp(KeyCode.leftArrow)) {
+            Debug.Log("Moved Left");
+        } else if (Input.GetKeyUp(KeyCode.rightArrow)) {
+            Debug.Log("Moved Right");
+        } else if (Input.GetKeyUp(KeyCode.downArrow)) {
+            Debug.Log("Soft Drop");
+        } else if (Input.GetKeyUp(KeyCode.z)) {
+            Debug.Log("Rotated Left");
+        } else if (Input.GetKeyUp(KeyCode.x)) {
+            Debug.Log("Rotated Right");
+        }*/
+    }
+}
